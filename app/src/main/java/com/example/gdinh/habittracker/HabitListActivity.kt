a5ea51c75905a89bbf60c0d141abe582879c57d6
Giang Dinh <gdinhvu@msudenver.edu>
2018-05-03 19:22:26 -0600

    Added habit list layout

--- app/src/main/java/com/example/gdinh/habittracker/HabitListActivity.kt
+++ app/src/main/java/com/example/gdinh/habittracker/HabitListActivity.kt
@@ -2,11 +2,21 @@ package com.example.gdinh.habittracker
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 
 class HabitListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_habit_list)
+        setSupportActionBar(findViewById(R.id.toolbar))
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_habit_list, menu)
+
+        return true
+    }
+
+
 }
