1b7857d6171af3fd255a3a4efa1b39e349e537ba
Giang Dinh <gdinhvu@msudenver.edu>
2018-05-03 19:04:03 -0600

    Added menu items to HabitTracker

---
+++ app/src/main/java/com/example/gdinh/habittracker/HabitListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gdinh.habittracker
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class HabitListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_habit_list)
+    }
+}
